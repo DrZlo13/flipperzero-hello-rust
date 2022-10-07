5d008e89edb7492f58a7bfd9073ae69f5bd3e07c
David Coles <coles.david@gmail.com>
2022-10-06 23:01:39 -0700

    Update to flipperzero-0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -10,40 +10,43 @@ use core::ptr;
 use core::time::Duration;
 
 extern crate flipperzero;
-use flipperzero::furi::{Stdout, sleep};
+use flipperzero::println;
+use flipperzero::furi::thread::sleep;
+use flipperzero::furi::sync::Mutex;
 use flipperzero_sys as sys;
 use flipperzero_sys::c_string;
-use flipperzero_sys::canvas::Canvas;
+use flipperzero_sys::gui::canvas::Canvas;
 use flipperzero_sys::gui::{RECORD_GUI, Gui, GuiLayer};
 
 /// View draw handler.
 pub extern "C" fn draw_callback(canvas: *mut Canvas, _context: *mut c_void) {
     unsafe {
-        sys::canvas::draw_str(canvas, 39, 31, c_string!("Hello, Rust!"));
+        sys::gui::canvas::draw_str(canvas, 39, 31, c_string!("Hello, Rust!"));
     }
 }
 
 /// Application entry point.
 #[no_mangle]
 pub extern "C" fn hello_rust_app(_args: *mut u8) -> i32 {
-    let mut stdout = Stdout;
-
-    write!(&mut stdout, "Hello, Rust! \u{1F980}\r\n").unwrap();
-    stdout.flush().unwrap();
+    let message = Mutex::new("Hello, Rust! \u{1F980}");
+    {
+        let message = message.lock().unwrap();
+        println!("{}", *message);
+    }
 
     unsafe {
-        let view_port = sys::view_port::alloc();
-        sys::view_port::draw_callback_set(view_port, draw_callback, ptr::null_mut());
+        let view_port = sys::gui::view_port::alloc();
+        sys::gui::view_port::draw_callback_set(view_port, draw_callback, ptr::null_mut());
 
-        let gui = sys::furi::record_open(RECORD_GUI) as *mut Gui;
+        let gui = sys::furi::record::open(RECORD_GUI) as *mut Gui;
         sys::gui::add_view_port(gui, view_port, GuiLayer::Fullscreen);
 
         sleep(Duration::from_secs(1));
 
-        sys::view_port::enabled_set(view_port, false);
+        sys::gui::view_port::enabled_set(view_port, false);
         sys::gui::remove_view_port(gui, view_port);
-        sys::furi::record_close(RECORD_GUI);
-        sys::view_port::free(view_port);
+        sys::furi::record::close(RECORD_GUI);
+        sys::gui::view_port::free(view_port);
     }
 
     0
